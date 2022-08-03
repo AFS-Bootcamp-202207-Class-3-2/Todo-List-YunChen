c08f3e2ffad2ef4134d144157d4cc8f7df05cf69
YunChen <1183728554@qq.com>
2022-08-04 00:51:11 +0800

    feat:change npm to yarn and introdule less css

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { BrowserRouter } from 'react-router-dom';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.less';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
